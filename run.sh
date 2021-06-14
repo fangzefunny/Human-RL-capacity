c2cccd50815c614415e01f0ada8395029b33668b
fangz5 <fangz5@rpi.edu>
2021-06-14 17:26:45 -0400

    fix a bug

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ declare models=('Pi_Rep_Grad')
 for dataset in "${datasets[@]}"; do 
     for model in "${models[@]}"; do 
         echo Data set=$dataset 14 Model=$model
-            python m1_fit_model.py -f=6 -c=3 -n=$model -d=$dataset
+            python m1_fit_model.py -f=6 -c=6 -n=$model -d=$dataset
     done
 done 
